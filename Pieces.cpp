29c473835a7961e1676048f74c1cf920521e5af6
Jan <jwilomski1@gmail.com>
2026-06-08 22:39:01 +0200

    Initial commit

---
+++ Pieces.cpp
@@ -0,0 +1,5 @@
+﻿//
+// Created by Jan on 08.06.2026.
+//
+
+#include "Pieces.h"
---
+++ Pieces.h
@@ -0,0 +1,25 @@
+﻿//
+// Created by Jan on 08.06.2026.
+//
+
+#ifndef CHESSENGINE_PIECES_H
+#define CHESSENGINE_PIECES_H
+
+
+enum class Piece {
+    None,
+    WhitePawn,
+    BlackPawn,
+    WhiteKnight,
+    BlackKnight,
+    WhiteBishop,
+    BlackBishop,
+    WhiteRook,
+    BlackRook,
+    WhiteQueen,
+    BlackQueen,
+    WhiteKing,
+    BlackKing
+};
+
+#endif //CHESSENGINE_PIECES_H
